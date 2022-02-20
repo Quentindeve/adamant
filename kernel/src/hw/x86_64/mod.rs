706c119dcd87d0f7da56816111cf887204e6f559
Quentincestino <quentindutilleul@pm.me>
2022-02-20 22:06:02 +0100

    x86_64: main: init texts to debug gdt/idt

--- kernel/src/hw/x86_64/mod.rs
+++ kernel/src/hw/x86_64/mod.rs
@@ -1,5 +1,7 @@
 use core::arch::asm;
 
+use self::com::ComPort;
+
 pub mod com;
 pub mod gdt;
 pub mod idt;
@@ -9,14 +11,15 @@ pub mod stivale2;
 
 /// The x86_64 arch entry point.
 extern "C" fn entry_point(_stivale_struct: &stivale_boot::v2::StivaleStruct) -> ! {
-    gdt::setup_gdt();
-    idt::setup_idt();
     com::init_serial(&com::ComPort::COM1);
 
-    let msg = b"Hello World !";
-    for c in msg {
-        com::putchar(&com::ComPort::COM1, *c as char);
-    }
+    com::write_text(ComPort::COM1, "Pre GDT");
+    gdt::setup_gdt();
+    com::write_text(ComPort::COM1, "Post GDT");
+
+    com::write_text(ComPort::COM1, "Pre IDT");
+    idt::setup_idt();
+    com::write_text(ComPort::COM1, "Post IDT");
 
     unsafe {
         asm!("int 0");
