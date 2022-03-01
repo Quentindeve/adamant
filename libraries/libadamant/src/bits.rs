cab086615b96cb3924f40db0c456a89164b1cf40
Quentincestino <quentindutilleul@pm.me>
2022-03-01 15:32:39 +0100

    libadamant: bits: started implementation of bitmap

---
+++ libraries/libadamant/src/bits.rs
@@ -0,0 +1,25 @@
+//! This module defines all the code to create and manipulate a bitmap.
+
+/// Structure representing a bitmap of [base; len[.
+pub struct Bits {
+    base: *mut u8,
+    len: usize,
+}
+
+impl Bits {
+    /// Creates a new bitmap.
+    /// Be careful: this function will panic if base is null and len is less than 1.
+    pub fn new(base: *mut u8, len: usize) -> Self {
+        assert_ne!(base, core::ptr::null_mut());
+        assert!(len >= 1);
+
+        Self { base, len }
+    }
+
+    /// Get the element at the specific index.
+    pub fn get(&self, index: usize) -> *mut u8 {
+        assert!(index <= self.len - 1);
+
+        ((self.base as usize) + index) as *mut u8
+    }
+}
