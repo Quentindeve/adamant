f52fab4eec9b379a0b778044ee337b6032b9bd4e
Quentin DUTILLEUL <quentin.dutilleul.etu@univ-lille.fr>
2022-05-09 16:18:14 +0200

    libadamant: bits: added a set method + utilitaries functions

--- libraries/libadamant/src/bits.rs
+++ libraries/libadamant/src/bits.rs
@@ -1,9 +1,10 @@
 //! This module defines all the code to create and manipulate a bitmap.
 
 /// Structure representing a bitmap of [base; len[.
+#[repr(C)]
 pub struct Bits {
-    base: *mut u8,
-    len: usize,
+    pub len: usize,
+    pub base: *mut u8,
 }
 
 impl Bits {
@@ -22,4 +23,29 @@ impl Bits {
 
         ((self.base as usize) + index) as *mut u8
     }
+
+    pub fn set(&mut self, index: usize, state: bool) {
+        let byte_index = byte_index(index);
+        let bit_index = bit_index(index);
+
+        if state {
+            unsafe {
+                *(self.get(byte_index)) |= 1 << bit_index;
+            }
+        } else {
+            unsafe {
+                *(self.get(byte_index)) &= !(1 << (bit_index));
+            }
+        }
+    }
+}
+
+#[inline(always)]
+pub fn byte_index(index: usize) -> usize {
+    index / 8
+}
+
+#[inline(always)]
+pub fn bit_index(index: usize) -> usize {
+    index % 8
 }
