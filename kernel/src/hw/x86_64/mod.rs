56675ca8b95fca8ebd1971be4e05c6c4a1f3acce
Quentin DUTILLEUL <quentin.dutilleul.etu@univ-lille.fr>
2022-05-09 16:17:01 +0200

    x86_64: static handover + initialize the pmm

--- kernel/src/hw/x86_64/mod.rs
+++ kernel/src/hw/x86_64/mod.rs
@@ -1,7 +1,9 @@
 use core::arch::asm;
 
+use crate::pmm;
+
 use self::com::ComPort;
-use libadamant::{log, print, println, boot::handover::Handover};
+use libadamant::{boot::handover::Handover, log, print, println};
 
 pub mod com;
 pub mod gdt;
@@ -11,6 +13,7 @@ pub mod port;
 pub mod stivale2;
 
 const COM_PORT: ComPort = ComPort::COM1;
+static mut HANDOVER: Handover = Handover::null();
 
 /// The x86_64 arch entry point.
 extern "C" fn entry_point(stivale_struct: &stivale_boot::v2::StivaleStruct) -> ! {
@@ -29,12 +32,17 @@ extern "C" fn entry_point(stivale_struct: &stivale_boot::v2::StivaleStruct) -> !
         asm!("int 0");
     }
 
-    let mut handover = Handover::null();
-    libadamant::boot::stivale2_to_handover(&stivale_struct, &mut handover);
+    libadamant::boot::stivale2_to_handover(&stivale_struct, unsafe { &mut HANDOVER });
 
-    for entry in handover.mmap {
-        println!("New entry ! Base = {}, End = {}, Type: {:?}", entry.begin, entry.end, entry.mmap_type);
+    for entry in unsafe { HANDOVER.mmap } {
+        println!(
+            "New entry ! Base = {}, End = {}, Type: {:?}",
+            entry.begin, entry.end, entry.mmap_type
+        );
     }
 
+    unsafe {
+        pmm::pmm_initialize(&HANDOVER.mmap);
+    }
     loop {}
 }
